5c628cefe45e0afcb80768196cb79b3f49afa0b6
hsi-tzu <t0953737036@gmail.com>
2021-07-10 15:38:28 +0800

    cpp檔裡的程式
    測試

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
+// Your First C++ Program
 
-
-#include <stdlib.h>
 #include <iostream>
-using namespace std;
-int main() { 
-	//using std::cout;
-	//using std::endl;
-	cout << "Hello, world" << endl;//前面已定义std,则后面可以直接调用，不必写std::cout...
-	//using std::cout;
-	//using std::endl;
-	//std::cout<<"hello world."<<std::endl;
-	//cout<<"hello world."<<endl;
-	system("pause");
-	return 0;
 
+int main() {
+    std::cout << "Hello World!";
+    return 0;
 }
