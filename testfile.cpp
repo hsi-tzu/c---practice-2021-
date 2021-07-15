2ee724ffb34af7f7f55251f4a5940bc2029d6078
hsi-tzu <t0953737036@gmail.com>
2021-07-15 14:38:01 +0800

    2021/7/15

---
+++ testfile.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <string> 
+using namespace std; 
+int main(){
+    int count1,count2;
+
+    cout<<"請輸入list1個數";
+    cin >> count1; //指定有幾個節點
+    //cout<<"\n";
+    cout <<"測試"<<count1;
+}
